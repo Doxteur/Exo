4099fa5ab6ce8cb859466c838da29cc6a7503cd0
Doxteur <boulji35530@gmail.com>
2024-03-25 19:39:11 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,100 @@
+// Exercice 1
+
+function exo1() {
+  // Retourner la valeur 10
+  return;
+}
+
+// Exercice 2
+
+function exo2() {
+  // Créer une variable a et lui assigner la valeur 5 puis retourner la valeur de a
+  return;
+}
+
+// Exercice 3
+
+function exo3() {
+  // Créer une variable a et lui assigner la valeur 5
+
+  // Créer une variable b et lui assigner la valeur 10
+
+  // Additionner a et b et retourner le résultat
+  return;
+}
+
+// Exercice 4
+function exo4(a) {
+  // Renvoyer le paramètre a
+  return;
+}
+
+// Exercice 5 loop
+function exo5() {
+  // Créer une variable a et lui assigner la valeur 0
+
+  // Créer une boucle qui incrémente a de 1 à 10
+
+  // Renvoyer a
+  return;
+}
+
+// Exercice 6
+function exo6(a) {
+  // Renvoyer vrai si a est supérieur egal à 10, sinon faux
+
+  return;
+}
+
+// Exercice 7
+function exo7(a) {
+  // Si a est un nombre pair, renvoyer vrai, sinon faux
+
+  return;
+}
+
+// Exercice 8 Tableau
+
+function exo8() {
+  // Créer un tableau contenant les valeurs 1, 2, 3, 4, 5
+
+  // Renvoyer le tableau
+
+  return;
+}
+
+// Exercice 9 Tableau
+
+function exo9() {
+  // Créer un tableau contenant les valeurs 1, 2, 3, 4, 5
+
+  // Renvoyer la première valeur du tableau
+  return;
+}
+
+// Exercice 10 Tableau
+
+function exo10() {
+  // Créer un tableau contenant les valeurs 1, 2, 3, 4, 5
+
+  // Renvoyer la taille du tableau
+  return;
+}
+
+// Exercice 11 Tableau
+
+function exo11() {
+  // Créer un tableau contenant les valeurs 1, 2, 3, 4, 5
+
+  // Renvoyer la dernière valeur du tableau, ne pas utiliser des []
+  return;
+}
+
+// Exercice 12 Object
+
+function exo12() {
+  // Créer un objet contenant les clés suivantes: nom, prenom, age
+
+  // Renvoyer l'objet
+  return;
+}
